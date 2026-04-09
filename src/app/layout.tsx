24bb00cdd70e9c6b803324e52a0a680f1c80ce74
강수훈 <kang@gangsuhun-ui-MacBookAir.local>
2026-04-08 19:41:49 -0600

    Remove all ads and add business email

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,18 +5,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="es">
       <head>
         {/* Monetag Vignette 광고 스크립트 */}
-        <Script
-          id="monetag-vignette"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(s){
-                s.dataset.zone='10848770';
-                s.src='https://n6wxm.com/vignette.min.js';
-              })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
-            `,
-          }}
-        />
+        
       </head>
       <body>{children}</body>
     </html>
